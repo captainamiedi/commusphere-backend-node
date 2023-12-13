e66110d7f61bd944b06af0318f82414bcd00fa67
Bright Amidiagbe <bright@nextdaysite.com>
2023-12-13 15:20:10 +0100

    update

--- src/controllers/metaController.js
+++ src/controllers/metaController.js
@@ -3,6 +3,14 @@ export default {
         try {
             console.log(req.body, 'request instagram webhook body');
             console.log(req, 'request instagram webhook');
+            if (
+                req.query['hub.mode'] == 'subscribe' &&
+                req.query['hub.verify_token'] == token
+              ) {
+                res.send(req.query['hub.challenge']);
+              } else {
+                res.sendStatus(400);
+              }
         } catch (error) {
             console.log(error);
         }
