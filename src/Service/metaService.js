2102ab3ce0a01d0d904494aa4886116a8cc19dc5
Bright Amidiagbe <bright.amidiagbe@churchpad.com>
2024-12-13 14:32:52 +0100

    remove file

--- src/Service/metaService.js
+++ src/Service/metaService.js
@@ -15,15 +15,7 @@ export const getPageAccessToken =async (userId, access_token) => {
 }
 export const getAppAccessToken =async () => {
     try {
-//         https://graph.facebook.com/oauth/access_token
-//   ?client_id={your-app-id}
-//   &client_secret={your-app-secret}
-//   &grant_type=client_credentials
-        // https://graph.facebook.com/{your-user-id}/accounts?access_token={user-access-token}
-        const response = await axios.get('https://graph.facebook.com/oauth/access_token?client_id=323926047109404&client_secret=0ee81c649ef73b43238b836bb2f80f56&grant_type=client_credentials')
-        console.log(response.data, 'response data');
-        console.log(response, 'response');
-        return response.data
+
     } catch (error) {
         
     }
